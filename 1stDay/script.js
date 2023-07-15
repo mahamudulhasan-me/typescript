d0b0c70c657c2d6ae603b959249e4faf2b38da55
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-07-15 13:54:10 +0600

    initial commit

---
+++ 1stDay/script.js
@@ -0,0 +1,3 @@
+console.log("Hello Typescript");
+var myName = "Mahmudul Hasan";
+console.log("My Name is ".concat(myName));
---
+++ 1stDay/script.ts
@@ -0,0 +1,4 @@
+console.log("Hello Typescript");
+
+const myName = "Mahmudul Hasan";
+console.log(`My Name is ${myName}`);
